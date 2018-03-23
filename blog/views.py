71cf440e9af3f12818d100b88669668a4bade12c
marce555 <marcelochaparrox@gmail.com>
2018-03-23 06:47:07 +0100

    reparation new

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_new', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_new.html', {'form': form})
